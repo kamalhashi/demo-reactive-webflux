cfac7579aa6b1fb702e322c56a8f40183b2d2cc4
Kamal Weheliye <kamal.weheliye@roamworks.com>
2018-10-09 09:52:30 +0400

    added filter

--- src/main/kotlin/com/demo/reactive/controller/TeamHandler.kt
+++ src/main/kotlin/com/demo/reactive/controller/TeamHandler.kt
@@ -1,15 +1,11 @@
 package com.demo.reactive.controller
 
 import com.demo.reactive.model.Team
-import com.demo.reactive.repository.TeamRepository
 import com.demo.reactive.service.APIService
-import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 import java.net.URI
 
@@ -20,16 +16,15 @@ class TeamHandler(val reactiveMongoTemplate: ReactiveMongoTemplate, val apiServi
             request
                     .bodyToMono(Team::class.java)
                     .map { team ->
-                        apiService.getPlayers()
-                                .collectList()
-                                .zipWith(team.toMono())
+                        apiService.getPlayers()  //get players from another service
+                                .filter{it-> it.id <= 3}  //Fitler and fetch only Chelsea FC Players
+                                .collectList() //collect all elements into list
+                                .zipWith(team.toMono()) // combine the players list into team, for furthure processing
                     }
                     .flatMap { it -> it }
                     .map { it ->
-                        Team.mergeGroupToMission(it.t1, it.t2)
+                        Team.mergeGroupToMission(it.t1, it.t2) //merging the players to their team, now  Hazard, Kante and Jorginho are Chelsea FC players
                     }
-                    .flatMap { reactiveMongoTemplate.save(it) }
+                    .flatMap { reactiveMongoTemplate.save(it) } // save date into database
                     .flatMap { it -> ServerResponse.created(URI.create("/team/" + it.id)).build() }
-
-
 }
